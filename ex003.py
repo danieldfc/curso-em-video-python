145c92b83a4c4929e3fcbe145391678806cfbdc7
Daniel Felizardo <daniel.david772@gmail.com>
2019-10-29 20:59:28 -0300

    feat: sum of two values

---
+++ ex003.py
@@ -0,0 +1,6 @@
+first_value = int(input('Digite um valor: '))
+second_value = int(input('Digite um valor: '))
+
+sum_total = (first_value + second_value)
+
+print('A soma entre {} e {} é igual a {}!'.format(first_value, second_value, sum_total))
