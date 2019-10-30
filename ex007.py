6571c4a950da6e03c9c0951901915dfccac09b1c
Daniel Felizardo <daniel.david772@gmail.com>
2019-10-29 21:35:02 -0300

    feat: media aritmética

---
+++ ex007.py
@@ -0,0 +1,6 @@
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+
+media = (nota1+nota2) / 2
+
+print('A média entre {:.1f} e {:.1f} é igual a {:.1f}'.format(nota1, nota2, media))
