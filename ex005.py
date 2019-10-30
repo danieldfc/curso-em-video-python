6cab7c6a5882480d4c34ef7c7f053a4d50d71fb2
Daniel Felizardo <daniel.david772@gmail.com>
2019-10-29 21:15:20 -0300

    feat: antecessor e sucessor

---
+++ ex005.py
@@ -0,0 +1,5 @@
+numeric_value = int(input('Digite um número: '))
+antecessor = numeric_value - 1
+sucessor = numeric_value + 1
+
+print('Analisando o valor {}, se antecessor é {} e o seu sucessor é {}'.format(numeric_value, antecessor, sucessor))
