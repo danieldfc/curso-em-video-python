23623fc73add739e407fb093c50f5be5a04c139b
Daniel Felizardo <daniel.david772@gmail.com>
2019-10-29 21:16:49 -0300

    chore: using a variable

--- ex005.py
+++ ex005.py
@@ -1,5 +1,3 @@
 numeric_value = int(input('Digite um número: '))
-antecessor = numeric_value - 1
-sucessor = numeric_value + 1
 
-print('Analisando o valor {}, se antecessor é {} e o seu sucessor é {}'.format(numeric_value, antecessor, sucessor))
+print('Analisando o valor {}, se antecessor é {} e o seu sucessor é {}'.format(numeric_value, (numeric_value - 1), (numeric_value + 1)))
