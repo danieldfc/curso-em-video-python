02f29ba69f62e98f8650cc55cd33c3b010c81212
Daniel Felizardo <daniel.david772@gmail.com>
2019-10-29 22:40:40 -0300

    feat: distance in metros converter

---
+++ ex008.py
@@ -0,0 +1,9 @@
+distance = float(input('Uma distância em metros: '))
+
+print('A medida de {}m corresponde a'.format(distance))
+print('{}km'.format(distance/1000))
+print('{}hm'.format(distance/100))
+print('{}dam'.format(distance/10))
+print('{}dm'.format(distance*10))
+print('{}cm'.format(distance*100))
+print('{}mm'.format(distance*1000))
